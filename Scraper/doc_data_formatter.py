f091bb48fdefef7772d4b8cc3c5845e2717979c2
mangobangoisme <mangobango7509@gmail.com>
2024-12-06 10:58:06 -0600

    created doc_data_formatter that actually works

--- Scraper/doc_data_formatter.py
+++ Scraper/doc_data_formatter.py
@@ -8,5 +8,11 @@ os.chdir(documents_dir)
 documents = os.listdir()
 
 for doc in documents:
-    doc_text = open(doc).read()
-    doc_title_split = doc.split("%")
+    if doc.endswith(".txt"):
+        doc_text = open(doc).read()
+        doc_title_split = re.split(r'[.%]', doc)
+        doc_title = doc_title_split[0]
+        doc_year = re.search(r"\d{4}", doc_title_split[-2]).group(0)
+        print(doc_title, doc_year)
+        with open("doc_data.csv", "a") as file:
+            file.write(f"\nDocuments/{doc},{doc_title},{doc_year}")
