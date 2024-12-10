3b177eb420a21374e8499497cde7c4263d4ddb95
Liam Sullivan <013353@mtka.org>
2024-12-10 16:54:55 -0600

    added progress bar for doc_data_formatter

--- Scraper/doc_data_formatter.py
+++ Scraper/doc_data_formatter.py
@@ -1,5 +1,6 @@
 import os
 import re
+from tqdm import tqdm
 
 documents_dir = "Documents"
 
@@ -7,15 +8,21 @@ os.chdir(documents_dir)
 
 documents = os.listdir()
 
-file = open("doc_data.csv", "w")
+file = open("_doc_data.csv", "w")
 file.write("filepath,title,year")
 
-for doc in documents:
+for i in tqdm(range(len(documents)), 
+               desc="Loading…", 
+               ascii=False, ncols=75):
+    doc=documents[i]
     if doc.endswith(".txt"):
-        doc_text = open(doc).read()
-        doc_title_split = re.split(r'[.%]', doc)
-        doc_title = doc_title_split[0]
-        doc_year = re.search(r"\d{4}", doc_title_split[-2]).group(0)
-        print(doc_title, doc_year)
-        with open("doc_data.csv", "a") as file:
-            file.write(f"\nDocuments/{doc},{doc_title},{doc_year}")
+        try:
+            doc_text = open(doc).read()
+            doc_title_split = re.split(r'[.%]', doc)
+            doc_title = doc_title_split[0]
+            doc_year = re.search(r"\d{4}", doc_title_split[-2]).group(0)
+            with open("_doc_data.csv", "a") as file:
+                file.write(f"\n\"Documents/{doc}\",\"{doc_title}\",{doc_year}")
+        except Exception as e:
+            print(e)
+            print(doc)
