087a9cd65564eef1563c280802ae81e1f5e64788
mangobangoisme <mangobango7509@gmail.com>
2024-12-10 10:55:20 -0600

    implemented train-test split with chunker

--- FR_Test/feature_representer.py
+++ FR_Test/feature_representer.py
@@ -1,5 +1,3 @@
-import chunker
-
 import os
 import torch
 from torch import nn
@@ -8,4 +6,16 @@ from transformers import BertTokenizer, BertModel, AdamW, get_linear_schedule_wi
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
 import pandas as pd
+from chunker import chunk_file
 
+data_df = pd.read_csv("Documents/doc_data.csv")
+
+dev = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+train, test = train_test_split(data_df, test_size=0.2)
+
+print(train)
+
+for i, row in train.iterrows():
+    print(row["filepath"])
+    print(chunk_file(row["filepath"]))
