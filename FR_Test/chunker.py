2e64285ca6762458ace10e889a313019a81addb0
mangobangoisme <mangobango7509@gmail.com>
2024-12-10 10:55:09 -0600

    chunker should work now

--- FR_Test/chunker.py
+++ FR_Test/chunker.py
@@ -1,32 +1,37 @@
 import os
 
 def is_paragraph(para):
-    return len(para) > 30 and len(para.split()) > 2
+    return len(para) > 10 and len(para.split()) > 2
 
 
-def separate_paragraphs(filepath):
+def chunk_paragraphs(filepath):
     if os.path.isfile(filepath):
         with open(filepath) as doc:
-            doc_text = doc.read()
-            doc_lines = doc_text.splitlines()
+            doc_lines = doc.readlines()
+            print(doc_lines)
+            # doc_lines = doc_text.splitlines()
 
             doc_paragraphs = []
 
             cur_paragraph = ""
             for line in doc_lines:
-                if line == "" and cur_paragraph != "":
+                ln = line.replace("\n", "")
+                if ln == "" and cur_paragraph != "":
                     if is_paragraph(cur_paragraph):
                         doc_paragraphs.append(cur_paragraph)
                     cur_paragraph = ""
                 else:
                     if cur_paragraph != "":
                         cur_paragraph += "\n"
-                    cur_paragraph += line
-                        
+                    cur_paragraph += ln
+            if is_paragraph(cur_paragraph):
+                doc_paragraphs.append(cur_paragraph)                  
     return doc_paragraphs
 
-def chunk(filepath, size=1):
-    paragraphs = separate_paragraphs(filepath)
+def chunk_file(filepath, size=1):
+    paragraphs = chunk_paragraphs(filepath)
+    
+    print(paragraphs)
     
     chunk = ""
     counter = 1
@@ -39,3 +44,5 @@ def chunk(filepath, size=1):
             chunk = ""
 
         counter += 1
+        
+    return chunks
