84f4117e4d50e69372f1245c08beaf7672dc9850
mangobangoisme <mangobango7509@gmail.com>
2024-12-19 20:59:41 -0600

    added preliminary BERT tokenizer

--- FR_Test/feature_representer.py
+++ FR_Test/feature_representer.py
@@ -1,21 +1,32 @@
-import os
 import torch
-from torch import nn
-from torch.utils.data import DataLoader, Dataset
-from transformers import BertTokenizer, BertModel, AdamW, get_linear_schedule_with_warmup
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report
+from transformers import BertTokenizerFast
 import pandas as pd
 from chunker import chunk_file
+from tqdm import tqdm
 
-data_df = pd.read_csv("Documents/doc_data.csv")
+data_df = pd.read_csv("Documents/_doc_data.csv")
 
 dev = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 train, test = train_test_split(data_df, test_size=0.2)
 
-print(train)
+chunks = []
 
-for i, row in train.iterrows():
-    print(row["filepath"])
-    print(chunk_file(row["filepath"]))
+for i, row in tqdm(train.iterrows(), total=len(train.index), desc="Chunking"):
+    chunks.append(chunk_file(row["filepath"]))
+    
+tokens = []
+
+bert = BertTokenizerFast.from_pretrained("google-bert/bert-base-uncased")
+
+for doc in tqdm(chunks, desc="Tokenizing"):
+    tokenized_doc = []
+    for chunk in doc:
+        
+        
+        
+        tokenized_chunk = bert.encode(chunk)
+        # print(tokenized_chunk)
+        # input()
+        tokenized_doc.append(tokenized_chunk)
