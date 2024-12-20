1dae5998dc1df07c5980f024c193fa7da38d5d60
Liam Sullivan <013353@mtka.org>
2024-12-20 10:38:02 -0600

    removed " and , from filepaths

--- Scraper/scraper_tools.py
+++ Scraper/scraper_tools.py
@@ -1,7 +1,7 @@
 import re
 
 def format_name(name):
-    return re.sub(r"[\/\.\<\>|\:&]", "_", name)[:150]
+    return re.sub(r"[\/\.\<\>|\:&\",]", "_", name)[:150]
 
 class colors:
     HEADER = '\033[95m'
@@ -13,3 +13,17 @@ class colors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
+def format_names():
+    import os
+    from tqdm import tqdm
+    
+    os.chdir("Documents")
+
+    for doc in tqdm(os.listdir()):
+        if doc.endswith(".txt"):
+            doc_text = open(doc).read()
+            doc_title_split = re.split(r'[.%]', doc)
+            doc_title = doc_title_split[0]
+            doc_year = re.search(r"\d{4}", doc_title_split[-2]).group(0)
+            os.rename(doc, f"{format_name(doc_title)} %{doc_year}.txt")
