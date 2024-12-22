a8205311cc7a36b8299b6f5cfd3edd6c290d25f4
mangobangoisme <mangobango7509@gmail.com>
2024-12-21 21:07:43 -0600

    chunk_words works

--- FR_Test/chunker.py
+++ FR_Test/chunker.py
@@ -4,6 +4,44 @@ import re
 def is_long(para):
     return len(para) > 10 and len(para.split()) > 2
 
+def chunk_words(filepath, num):
+    if os.path.isfile(filepath):
+        with open(filepath, encoding="utf-8") as doc_file:
+            words = re.split(r"\s+", doc_file.read())
+            
+            split_words = []
+            
+            cur_split = ""
+            
+            for i in range(len(words)):
+                if i % num == 0 and len(cur_split) > 0:
+                    split_words.append(cur_split)
+                    cur_split = ""
+                else:
+                    cur_split += words[i] + " "
+            
+            return split_words
+                    
+    else:
+        raise Exception("Filepath does not lead to a file.")
+
+def chunk_sentences(filepath):
+    if os.path.isfile(filepath):
+        with open(filepath, encoding="utf-8") as doc_file:
+            doc = re.sub("\n+", "", doc_file.read())
+            
+            split_doc = re.split(r"([!\.?](?: |$))", doc)
+            
+            doc_sentences = []
+            
+            i = 0
+            while i < len(split_doc)-1:
+                doc_sentences.append(split_doc[i] + split_doc[i+1])
+                i += 2
+
+            return doc_sentences
+    else:
+        raise Exception("Filepath does not lead to a file.")
 
 def chunk_paragraphs(filepath):
     if os.path.isfile(filepath):
@@ -24,41 +62,33 @@ def chunk_paragraphs(filepath):
                         cur_paragraph += "\n"
                     cur_paragraph += ln
             if is_long(cur_paragraph):
-                doc_paragraphs.append(cur_paragraph)             
+                doc_paragraphs.append(cur_paragraph)
+                             
             return doc_paragraphs
     else:
         raise Exception("Filepath does not lead to a file.")
+          
 
-def chunk_sentences(filepath):
-    if os.path.isfile(filepath):
-        with open(filepath, encoding="utf-8") as doc_file:
-            doc = re.sub("\n+", "", doc_file.read())
-            
-            split_doc = re.split(r"([!\.?](?: |$))", doc)
-            
-            doc_sentences = []
-            
-            i = 0
-            while i < len(split_doc)-1:
-                doc_sentences.append(split_doc[i] + split_doc[i+1])
-                i += 2
-    else:
-        raise Exception("Filepath does not lead to a file.")
-            
-
-def chunk_file(filepath, size=1):
-    paragraphs = chunk_paragraphs(filepath)
+def chunk_file(filepath, size):
     
-    chunk = ""
-    counter = 1
-    chunks = []
-    for para in paragraphs:
-        chunk += para
-        
-        if counter % size == 0:
-            chunks.append(chunk)
+    match size:
+        case "sentence":
+            return chunk_sentences(filepath)
+        case "paragraph":
+            paragraphs = chunk_paragraphs(filepath)
+            
             chunk = ""
+            counter = 1
+            chunks = []
+            for para in paragraphs:
+                chunk += para
+                
+                if counter % size == 0:
+                    chunks.append(chunk)
+                    chunk = ""
 
-        counter += 1
-        
-    return chunks
+                counter += 1
+                
+            return chunks
+        case _:
+            return chunk_words(filepath, size)
--- FR_Test/feature_representer.py
+++ FR_Test/feature_representer.py
@@ -1,3 +1,5 @@
+print("Importing packages... ", end="")
+
 import torch
 from sklearn.model_selection import train_test_split
 from transformers import BertTokenizerFast
@@ -5,16 +7,18 @@ import pandas as pd
 from chunker import chunk_file
 from tqdm import tqdm
 
+print("DONE!")
+
 data_df = pd.read_csv("Documents/_doc_data.csv")
 
 dev = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-train, test = train_test_split(data_df, test_size=0.2)
+train, test = train_test_split(data_df, test_size=0.1)
 
 chunks = []
 
 for i, row in tqdm(train.iterrows(), total=len(train.index), desc="Chunking"):
-    chunks.append(chunk_file(row["filepath"]))
+    chunks.append(chunk_file(row["filepath"], 200))
     
 tokens = []
 
@@ -23,10 +27,5 @@ bert = BertTokenizerFast.from_pretrained("google-bert/bert-base-uncased")
 for doc in tqdm(chunks, desc="Tokenizing"):
     tokenized_doc = []
     for chunk in doc:
-        
-        
-        
         tokenized_chunk = bert.encode(chunk)
-        # print(tokenized_chunk)
-        # input()
         tokenized_doc.append(tokenized_chunk)
