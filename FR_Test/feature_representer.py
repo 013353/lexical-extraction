bb070472e74d5f2f6f8033198c0475397aafa80d
mangobangoisme <mangobango7509@gmail.com>
2024-12-23 22:35:34 -0600

    tf-idf function averages values

--- FR_Test/feature_representer.py
+++ FR_Test/feature_representer.py
@@ -1,6 +1,9 @@
 print("Importing packages... ", end="")
 
 import math
+from statistics import mean
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -15,11 +18,11 @@ data_df = pd.read_csv("Documents/_doc_data.csv")
 
 dev = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-train, test = train_test_split(data_df, test_size=0.2)
+train, test = train_test_split(data_df, test_size=0.8)
 
 vectorizer = TfidfVectorizer()
 
-def tf_idf(corpus, weights={}):
+def tf_idf(corpus):
     string_corpus = []
     for doc in corpus:
         doc_string = ""
@@ -31,9 +34,23 @@ def tf_idf(corpus, weights={}):
     
     tfidf_df = pd.DataFrame(tfidf_vector.toarray(), columns=vectorizer.get_feature_names_out())
     
-    print(len(string_corpus))
-    print(tfidf_vector)
-    print(tfidf_df)
+    token_df = tfidf_df.transpose()
+    
+    weights = {}
+    for token, data in tqdm(token_df.iterrows(), total=len(token_df.index), desc="Averaging"):
+        weights[token] = mean(data)
+    
+    print(weights)
+    
+    x = []
+    y = []
+    
+    for key, value in weights.items():
+        x.append(key)
+        y.append(value)
+
+    plt.plot(x, y, "o")
+    plt.show()
     
 def tokenize(dataset, mode, size, model):
     chunks = []
