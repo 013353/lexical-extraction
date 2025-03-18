2524465f50a27db385a851d023ca4cc56679622a
Liam Sullivan <013353@mtka.org>
2025-03-18 10:57:43 -0500

    added data visualizer for outputs

--- data_visualizer.py
+++ data_visualizer.py
@@ -1,10 +1,9 @@
-import os
-from tqdm import tqdm
-import pandas as pd
-from transformers import BertTokenizerFast
-from matplotlib import pyplot as plt
-
 def visualize_profiles(model):
+    import os
+    from tqdm import tqdm
+    import pandas as pd
+    from transformers import BertTokenizerFast
+    from matplotlib import pyplot as plt
     profile_files = os.listdir("FR_Test/profiles")
     print("PROFILES:", profile_files)
 
@@ -22,6 +21,45 @@ def visualize_profiles(model):
     # for profile in profiles:
     #     print(profile)
 
+def visualize_output():
+    import numpy as np
+    from sklearn.datasets import load_digits
+    from sklearn.model_selection import train_test_split
+    from sklearn.preprocessing import StandardScaler
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    import pandas as pd
+    import umap
+    sns.set_theme(style='white', context='notebook', rc={'figure.figsize':(14,10)})
+
+    df = pd.read_pickle("train_outputs.pickle").sample(frac=0.01)
+    print(df)
+    reducer = umap.UMAP()
+    data = df.iloc[:, 0].tolist()
+    years = df.iloc[:, 1]
+    del df
+    print(1)
+    # print(data)
+    scaled_data = StandardScaler().fit_transform(data)
+    print(2)
+    del data
+    embedding = reducer.fit_transform(scaled_data)
+    print(embedding.shape)
+
+    palette = sns.color_palette("nipy_spectral", 23)
+
+    plt.scatter(
+        embedding[:, 0],
+        embedding[:, 1],
+        c=years,
+        cmap="summer"
+    )
+    plt.colorbar()
+    # sns.palplot(palette)
+    plt.gca().set_aspect('equal', 'datalim')
+    plt.title('UMAP projection of outputs', fontsize=24)
+    plt.savefig("output.png")
+    plt.show()
+
 if __name__ == "__main__":
-    bert = BertTokenizerFast.from_pretrained("google-bert/bert-base-uncased")
-    visualize_profiles(bert)
+    visualize_output()
