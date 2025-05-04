b9bf9ca3ed19201a3cd03d79f7cdec417d81d71d
lvsull <mangobango7509@gmail.com>
2025-05-04 16:59:27 -0500

    add confusion_matrices

--- data_visualizer.py
+++ data_visualizer.py
@@ -1,3 +1,6 @@
+from functools import lru_cache
+
+
 def csv_to_pickle(csv_file: str, sep=";", pickle_file=None):
     import pandas as pd
     if not pickle_file:
@@ -58,69 +61,74 @@ def visualize_output(file: str):
     plt.savefig("output.png")
     plt.show()
 
+@lru_cache
+def cmatrix_df(file: str):
+    import pandas as pd
+    from tqdm import tqdm
+    data = pd.read_pickle(file)
+    num_tests = len(data.index)
+    data.sort_values(by=["estimate", "expected"], ignore_index=True, inplace=True)
+    matrix = pd.DataFrame(0, columns=sorted(list(set(data["expected"].tolist()))),
+                          index=sorted(list(set(data["expected"].tolist()))))
+
+    for i, row in tqdm(data.iterrows(), total=len(data.index)):
+        matrix.loc[row["expected"], row["estimate"]] += 1
+
+    def to_ratio(freq: int) -> float:
+        return round((freq / num_tests), 3)
+
+    matrix = matrix.map(to_ratio)
+
+    print(matrix)
+    return matrix
+
 def confusion_matrix(file: str, name: str):
     import pandas as pd
     import seaborn as sns
     import matplotlib.pyplot as plt
     from tqdm import tqdm
-    data = pd.read_pickle(file)
-    num_tests = len(data.index)
-    data.sort_values(by=["estimate", "expected"], ignore_index=True, inplace=True)
-    matrix = pd.DataFrame(0, columns=sorted(list(set(data["expected"].tolist()))), index=sorted(list(set(data["expected"].tolist()))))
-    
-    for i, row in tqdm(data.iterrows(), total=len(data.index)):
-        matrix.loc[row["expected"], row["estimate"]] += 1
-    
-    def to_ratio(freq: int) -> float:
-        return round((freq / num_tests), 3)
-    
-    matrix = matrix.map(to_ratio)
-    
-    print(matrix)
+    matrix = cmatrix_df(file)
     
     plt.figure(figsize=(12, 9))
-    # sns.set_theme(
-    #     font_scale=0.5,
-    #     )
     
-    ax = sns.heatmap(matrix, cmap="viridis", annot=True, square=True, annot_kws={"fontsize": 6}, cbar_kws={"label": "Ratio of Total Tests"})
+    ax = sns.heatmap(matrix, cmap="viridis", square=True, cbar_kws={"label": "Ratio of Total Tests"}, vmin=0, vmax=0.03)
     # ax.set(xlabel="Estimated Time Periods", ylabel="Actual Time Periods")
     ax.set_xlabel("Predicted Time Periods", fontsize=10)
     ax.set_ylabel("Actual Time Periods", fontsize=10)
     ax.set_title(f"Confusion Matrix of {name} Model", fontsize=20)
-    plt.savefig(f"{name}_cmatrix.png", dpi=2400)
-    # plt.show()
-    
+    plt.savefig(f"{name}_cmatrix.png", dpi=1200, format="png")
+    plt.show()
+
 def confusion_matrices(files: dict):
     import pandas as pd
     import seaborn as sns
     import matplotlib.pyplot as plt
-    from tqdm import tqdm
-    
+
+    def draw_heatmap(data, **kwargs):
+        data = data.drop(columns="amt-removed")
+        sns.heatmap(data, square=True, cbar=False, vmin=0, vmax=0.03, cmap="viridis", **kwargs)
+
+    long_df = pd.DataFrame(columns=range(1800, 2010, 10))
+
     for name in files:
-        file = files[name]
-        data = pd.read_pickle(file)
-        num_tests = len(data.index)
-        data.sort_values(by=["estimate", "expected"], ignore_index=True, inplace=True)
-        matrix = pd.DataFrame(0, columns=sorted(list(set(data["expected"].tolist()))), index=sorted(list(set(data["expected"].tolist()))))
-        
-        for i, row in tqdm(data.iterrows(), total=len(data.index)):
-            matrix.loc[row["expected"], row["estimate"]] += 1
-        
-        def to_ratio(freq: int) -> float:
-            return round((freq / num_tests), 3)
-        
-        matrix = matrix.map(to_ratio)
-        
-        print(matrix)
-        
-        ax = sns.heatmap(matrix, cmap="viridis", annot=True, square=True, annot_kws={"fontsize": 6}, cbar_kws={"label": "Ratio of Total Tests"})
-        # ax.set(xlabel="Estimated Time Periods", ylabel="Actual Time Periods")
-        ax.set_xlabel("Predicted Time Periods", fontsize=10)
-        ax.set_ylabel("Actual Time Periods", fontsize=10)
-        ax.set_title(f"Confusion Matrix of {name} Model", fontsize=20)
-    
-    plt.show()
+        matrix = cmatrix_df(files[name])
+        matrix["amt-removed"] = [name] * len(matrix)
+
+        long_df = pd.concat([long_df, matrix])
+
+    print(long_df)
+
+    g = sns.FacetGrid(long_df, col="amt-removed", sharey=False)
+    g.map_dataframe(draw_heatmap)
+    g.set_axis_labels("Predicted Time Periods", "Actual Time Periods")
+    g.set_titles("{col_name}")
+
+    sns.color_palette("viridis", as_cmap=True)
+
+    plt.tight_layout()
+    plt.savefig("heatmaps.png", dpi=4800)
+    plt.savefig("heatmaps.svg")
+    plt.show(dpi=2400)
 
 def graph_results(file: str):
     import pandas as pd
@@ -152,12 +160,12 @@ def graph_results(file: str):
     
     fig = plt.figure()
     sns.set_theme(style="whitegrid")
-    ax = sns.barplot(data, x="Amount Removed", y="Accuracy", hue="Metric", errorbar="se", palette="viridis_r", err_kws={"linewidth": 1, "color": "#000000"}, capsize=0.25)
+    ax = sns.barplot(data, x="Amount Removed", y="Accuracy", hue="Metric", errorbar="sd", palette="viridis_r", err_kws={"linewidth": 1, "color": "#000000"}, capsize=0.25)
     sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
     ax.set_title("Accuracy of Lexical Extraction Models")
     ax.set_xticks(ax.get_xticks(), ["BASE", 0.0625, 0.125, 0.25, 0.5])
-    # plt.show()
-    fig.savefig('accs.png', bbox_inches='tight', dpi=2400)
+    plt.show()
+    # fig.savefig('accs.png', bbox_inches='tight', dpi=2400)
     
 def head(file: str):
     import pandas as pd
@@ -165,4 +173,4 @@ def head(file: str):
     pd.read_pickle(file).iloc[:10].to_csv("head.csv")
 
 if __name__ == "__main__":
-    head("FR_Test/BP1T0/train_outputs.pickle")
+    pass
